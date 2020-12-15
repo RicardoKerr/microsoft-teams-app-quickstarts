1a1c94add44fa6215b9de8b5b360c3787dd0349f
Allen Snow <allsnow@microsoft.com>
2020-12-15 15:34:36 -0800

    Update samples to work with new toolkits

--- tabs/personal-sso/service/server-error.js
+++
@@ -1,9 +0,0 @@
-class ServerError extends Error {
-  constructor(statusCode, statusMessage) {
-    super(`ServerError: ${statusCode} - ${statusMessage}`);
-    this.statusCode = statusCode;
-    this.statusMessage = statusMessage;
-  }
-}
-
-module.exports = ServerError;
